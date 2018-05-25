2c7e97ce663ff82c49656bca3048df0fdd83c5f9
Olivier Poitrey <rs@rhapsodyk.net>
2018-05-24 20:12:57 -0700

    Remove dep on testify

--- id_test.go
+++ id_test.go
@@ -1,14 +1,14 @@
 package xid
 
 import (
+	"bytes"
 	"encoding/json"
+	"errors"
+	"fmt"
+	"reflect"
 	"testing"
 	"time"
-
-	"github.com/stretchr/testify/assert"
 )
-
-const strInvalidID = "xid: invalid ID"
 
 type IDParts struct {
 	id        ID
@@ -44,10 +44,20 @@ var IDs = []IDParts{
 
 func TestIDPartsExtraction(t *testing.T) {
 	for i, v := range IDs {
-		assert.Equal(t, v.id.Time(), time.Unix(v.timestamp, 0), "#%d timestamp", i)
-		assert.Equal(t, v.id.Machine(), v.machine, "#%d machine", i)
-		assert.Equal(t, v.id.Pid(), v.pid, "#%d pid", i)
-		assert.Equal(t, v.id.Counter(), v.counter, "#%d counter", i)
+		t.Run(fmt.Sprintf("Test%d", i), func(t *testing.T) {
+			if got, want := v.id.Time(), time.Unix(v.timestamp, 0); got != want {
+				t.Errorf("Time() = %v, want %v", got, want)
+			}
+			if got, want := v.id.Machine(), v.machine; !bytes.Equal(got, want) {
+				t.Errorf("Machine() = %v, want %v", got, want)
+			}
+			if got, want := v.id.Pid(), v.pid; got != want {
+				t.Errorf("Pid() = %v, want %v", got, want)
+			}
+			if got, want := v.id.Counter(), v.counter; got != want {
+				t.Errorf("Counter() = %v, want %v", got, want)
+			}
+		})
 	}
 }
 
@@ -63,37 +73,54 @@ func TestNew(t *testing.T) {
 		// Test for uniqueness among all other 9 generated ids
 		for j, tid := range ids {
 			if j != i {
-				assert.NotEqual(t, id, tid, "Generated ID is not unique")
+				if id.Compare(tid) == 0 {
+					t.Errorf("generated ID is not unique (%d/%d)", i, j)
+				}
 			}
 		}
 		// Check that timestamp was incremented and is within 30 seconds of the previous one
 		secs := id.Time().Sub(prevID.Time()).Seconds()
-		assert.Equal(t, (secs >= 0 && secs <= 30), true, "Wrong timestamp in generated ID")
+		if secs < 0 || secs > 30 {
+			t.Error("wrong timestamp in generated ID")
+		}
 		// Check that machine ids are the same
-		assert.Equal(t, id.Machine(), prevID.Machine())
+		if !bytes.Equal(id.Machine(), prevID.Machine()) {
+			t.Error("machine ID not equal")
+		}
 		// Check that pids are the same
-		assert.Equal(t, id.Pid(), prevID.Pid())
+		if id.Pid() != prevID.Pid() {
+			t.Error("pid not equal")
+		}
 		// Test for proper increment
-		delta := int(id.Counter() - prevID.Counter())
-		assert.Equal(t, delta, 1, "Wrong increment in generated ID")
+		if got, want := int(id.Counter()-prevID.Counter()), 1; got != want {
+			t.Errorf("wrong increment in generated ID, delta=%v, want %v", got, want)
+		}
 	}
 }
 
 func TestIDString(t *testing.T) {
 	id := ID{0x4d, 0x88, 0xe1, 0x5b, 0x60, 0xf4, 0x86, 0xe4, 0x28, 0x41, 0x2d, 0xc9}
-	assert.Equal(t, "9m4e2mr0ui3e8a215n4g", id.String())
+	if got, want := id.String(), "9m4e2mr0ui3e8a215n4g"; got != want {
+		t.Errorf("String() = %v, want %v", got, want)
+	}
 }
 
 func TestFromString(t *testing.T) {
-	id, err := FromString("9m4e2mr0ui3e8a215n4g")
-	assert.NoError(t, err)
-	assert.Equal(t, ID{0x4d, 0x88, 0xe1, 0x5b, 0x60, 0xf4, 0x86, 0xe4, 0x28, 0x41, 0x2d, 0xc9}, id)
+	got, err := FromString("9m4e2mr0ui3e8a215n4g")
+	if err != nil {
+		t.Fatal(err)
+	}
+	want := ID{0x4d, 0x88, 0xe1, 0x5b, 0x60, 0xf4, 0x86, 0xe4, 0x28, 0x41, 0x2d, 0xc9}
+	if got != want {
+		t.Errorf("FromString() = %v, want %v", got, want)
+	}
 }
 
 func TestFromStringInvalid(t *testing.T) {
-	id, err := FromString("invalid")
-	assert.EqualError(t, err, strInvalidID)
-	assert.Equal(t, ID{}, id)
+	_, err := FromString("invalid")
+	if err != ErrInvalidID {
+		t.Errorf("FromString(invalid) err=%v, want %v", err, ErrInvalidID)
+	}
 }
 
 type jsonType struct {
@@ -105,56 +132,88 @@ func TestIDJSONMarshaling(t *testing.T) {
 	id := ID{0x4d, 0x88, 0xe1, 0x5b, 0x60, 0xf4, 0x86, 0xe4, 0x28, 0x41, 0x2d, 0xc9}
 	v := jsonType{ID: &id, Str: "test"}
 	data, err := json.Marshal(&v)
-	assert.NoError(t, err)
-	assert.Equal(t, `{"ID":"9m4e2mr0ui3e8a215n4g","Str":"test"}`, string(data))
+	if err != nil {
+		t.Fatal(err)
+	}
+	if got, want := string(data), `{"ID":"9m4e2mr0ui3e8a215n4g","Str":"test"}`; got != want {
+		t.Errorf("json.Marshal() = %v, want %v", got, want)
+	}
 }
 
 func TestIDJSONUnmarshaling(t *testing.T) {
 	data := []byte(`{"ID":"9m4e2mr0ui3e8a215n4g","Str":"test"}`)
 	v := jsonType{}
 	err := json.Unmarshal(data, &v)
-	assert.NoError(t, err)
-	assert.Equal(t, ID{0x4d, 0x88, 0xe1, 0x5b, 0x60, 0xf4, 0x86, 0xe4, 0x28, 0x41, 0x2d, 0xc9}, *v.ID)
+	if err != nil {
+		t.Fatal(err)
+	}
+	want := ID{0x4d, 0x88, 0xe1, 0x5b, 0x60, 0xf4, 0x86, 0xe4, 0x28, 0x41, 0x2d, 0xc9}
+	if got := *v.ID; got.Compare(want) != 0 {
+		t.Errorf("json.Unmarshal() = %v, want %v", got, want)
+	}
+
 }
 
 func TestIDJSONUnmarshalingError(t *testing.T) {
 	v := jsonType{}
 	err := json.Unmarshal([]byte(`{"ID":"9M4E2MR0UI3E8A215N4G"}`), &v)
-	assert.EqualError(t, err, strInvalidID)
+	if err != ErrInvalidID {
+		t.Errorf("json.Unmarshal() err=%v, want %v", err, ErrInvalidID)
+	}
 	err = json.Unmarshal([]byte(`{"ID":"TYjhW2D0huQoQS"}`), &v)
-	assert.EqualError(t, err, strInvalidID)
+	if err != ErrInvalidID {
+		t.Errorf("json.Unmarshal() err=%v, want %v", err, ErrInvalidID)
+	}
 	err = json.Unmarshal([]byte(`{"ID":"TYjhW2D0huQoQS3kdk"}`), &v)
-	assert.EqualError(t, err, strInvalidID)
+	if err != ErrInvalidID {
+		t.Errorf("json.Unmarshal() err=%v, want %v", err, ErrInvalidID)
+	}
 }
 
 func TestIDDriverValue(t *testing.T) {
 	id := ID{0x4d, 0x88, 0xe1, 0x5b, 0x60, 0xf4, 0x86, 0xe4, 0x28, 0x41, 0x2d, 0xc9}
-	data, err := id.Value()
-	assert.NoError(t, err)
-	assert.Equal(t, "9m4e2mr0ui3e8a215n4g", data)
+	got, err := id.Value()
+	if err != nil {
+		t.Fatal(err)
+	}
+	if want := "9m4e2mr0ui3e8a215n4g"; got != want {
+		t.Errorf("Value() = %v, want %v", got, want)
+	}
 }
 
 func TestIDDriverScan(t *testing.T) {
-	id := ID{}
-	err := id.Scan("9m4e2mr0ui3e8a215n4g")
-	assert.NoError(t, err)
-	assert.Equal(t, ID{0x4d, 0x88, 0xe1, 0x5b, 0x60, 0xf4, 0x86, 0xe4, 0x28, 0x41, 0x2d, 0xc9}, id)
+	got := ID{}
+	err := got.Scan("9m4e2mr0ui3e8a215n4g")
+	if err != nil {
+		t.Fatal(err)
+	}
+	want := ID{0x4d, 0x88, 0xe1, 0x5b, 0x60, 0xf4, 0x86, 0xe4, 0x28, 0x41, 0x2d, 0xc9}
+	if got.Compare(want) != 0 {
+		t.Errorf("Scan() = %v, want %v", got, want)
+	}
 }
 
 func TestIDDriverScanError(t *testing.T) {
 	id := ID{}
-	err := id.Scan(0)
-	assert.EqualError(t, err, "xid: scanning unsupported type: int")
-	err = id.Scan("0")
-	assert.EqualError(t, err, strInvalidID)
+	if got, want := id.Scan(0), errors.New("xid: scanning unsupported type: int"); !reflect.DeepEqual(got, want) {
+		t.Errorf("Scan() err=%v, want %v", got, want)
+	}
+	if got, want := id.Scan("0"), ErrInvalidID; got != want {
+		t.Errorf("Scan() err=%v, want %v", got, want)
+	}
 }
 
 func TestIDDriverScanByteFromDatabase(t *testing.T) {
-	id := ID{}
+	got := ID{}
 	bs := []byte("9m4e2mr0ui3e8a215n4g")
-	err := id.Scan(bs)
-	assert.NoError(t, err)
-	assert.Equal(t, ID{0x4d, 0x88, 0xe1, 0x5b, 0x60, 0xf4, 0x86, 0xe4, 0x28, 0x41, 0x2d, 0xc9}, id)
+	err := got.Scan(bs)
+	if err != nil {
+		t.Fatal(err)
+	}
+	want := ID{0x4d, 0x88, 0xe1, 0x5b, 0x60, 0xf4, 0x86, 0xe4, 0x28, 0x41, 0x2d, 0xc9}
+	if got.Compare(want) != 0 {
+		t.Errorf("Scan() = %v, want %v", got, want)
+	}
 }
 
 func BenchmarkNew(b *testing.B) {
@@ -215,39 +274,44 @@ func TestID_IsNil(t *testing.T) {
 		},
 	}
 	for _, tt := range tests {
-		assert.Equal(t, tt.id.IsNil(), tt.want)
+		tt := tt
+		t.Run(tt.name, func(t *testing.T) {
+			if got, want := tt.id.IsNil(), tt.want; got != want {
+				t.Errorf("IsNil() = %v, want %v", got, want)
+			}
+		})
 	}
 }
 
 func TestNilID(t *testing.T) {
-	var id ID
-	nilid := NilID()
-	assert.Equal(t, id, nilid)
+	got := ID{}
+	if want := NilID(); !reflect.DeepEqual(got, want) {
+		t.Error("NilID() not equal ID{}")
+	}
 }
 
 func TestNilID_IsNil(t *testing.T) {
-	assert.True(t, NilID().IsNil())
-}
-
-func TestID_Bytes(t *testing.T) {
-	id := New()
-	underlying := [rawLen]byte(id)
-	b := id.Bytes()
-	for i := range underlying {
-		assert.Equal(t, underlying[i], b[i])
+	if !NilID().IsNil() {
+		t.Error("NilID().IsNil() is not true")
 	}
 }
 
 func TestFromBytes_Invariant(t *testing.T) {
-	id := New()
-	b, err := FromBytes(id.Bytes())
-	assert.NoError(t, err)
-	assert.Equal(t, b, id)
+	want := New()
+	got, err := FromBytes(want.Bytes())
+	if err != nil {
+		t.Fatal(err)
+	}
+	if got.Compare(want) != 0 {
+		t.Error("FromBytes(id.Bytes()) != id")
+	}
 }
 
 func TestFromBytes_InvalidBytes(t *testing.T) {
-	cases := []struct{
-		length int; shouldFail bool} {
+	cases := []struct {
+		length     int
+		shouldFail bool
+	}{
 		{11, true},
 		{12, false},
 		{13, true},
@@ -255,50 +319,54 @@ func TestFromBytes_InvalidBytes(t *testing.T) {
 	for _, c := range cases {
 		b := make([]byte, c.length, c.length)
 		_, err := FromBytes(b)
-		if c.shouldFail {
-			assert.Error(t, err, "Length %d should fail.", c.length)
-		} else {
-			assert.NoError(t, err, "Length %d should not fail.", c.length)
+		if got, want := err != nil, c.shouldFail; got != want {
+			t.Errorf("FromBytes() error got %v, want %v", got, want)
 		}
 	}
 }
 
 func TestID_Compare(t *testing.T) {
-	pairs := []struct{
-		left ID
-		right ID
+	pairs := []struct {
+		left     ID
+		right    ID
 		expected int
-	} {
+	}{
 		{IDs[1].id, IDs[0].id, -1},
 		{ID{0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00}, IDs[2].id, -1},
 		{IDs[0].id, IDs[0].id, 0},
 	}
 	for _, p := range pairs {
-		assert.Equal(
-			t, p.expected, p.left.Compare(p.right),
-			"%s Compare to %s should return %d", p.left, p.right, p.expected,
-		)
-		assert.Equal(
-			t, -1 * p.expected, p.right.Compare(p.left),
-			"%s Compare to %s should return %d", p.right, p.left, - 1 * p.expected,
-		)
+		if p.expected != p.left.Compare(p.right) {
+			t.Errorf("%s Compare to %s should return %d", p.left, p.right, p.expected)
+		}
+		if -1*p.expected != p.right.Compare(p.left) {
+			t.Errorf("%s Compare to %s should return %d", p.right, p.left, -1*p.expected)
+		}
 	}
 }
 
 var IDList = []ID{IDs[0].id, IDs[1].id, IDs[2].id}
 
-
 func TestSorter_Len(t *testing.T) {
-	assert.Equal(t, 0, sorter([]ID{}).Len())
-	assert.Equal(t, 3, sorter(IDList).Len())
+	if got, want := sorter([]ID{}).Len(), 0; got != want {
+		t.Errorf("Len() %v, want %v", got, want)
+	}
+	if got, want := sorter(IDList).Len(), 3; got != want {
+		t.Errorf("Len() %v, want %v", got, want)
+	}
 }
-
 
 func TestSorter_Less(t *testing.T) {
 	sorter := sorter(IDList)
-	assert.True(t, sorter.Less(1, 0))
-	assert.False(t, sorter.Less(2, 1))
-	assert.False(t, sorter.Less(0, 0))
+	if !sorter.Less(1, 0) {
+		t.Errorf("Less(1, 0) not true")
+	}
+	if sorter.Less(2, 1) {
+		t.Errorf("Less(2, 1) true")
+	}
+	if sorter.Less(0, 0) {
+		t.Errorf("Less(0, 0) true")
+	}
 }
 
 func TestSorter_Swap(t *testing.T) {
@@ -308,10 +376,16 @@ func TestSorter_Swap(t *testing.T) {
 	}
 	sorter := sorter(ids)
 	sorter.Swap(0, 1)
-	assert.Equal(t, ids[0], IDList[1])
-	assert.Equal(t, ids[1], IDList[0])
+	if got, want := ids[0], IDList[1]; !reflect.DeepEqual(got, want) {
+		t.Error("ids[0] != IDList[1]")
+	}
+	if got, want := ids[1], IDList[0]; !reflect.DeepEqual(got, want) {
+		t.Error("ids[1] != IDList[0]")
+	}
 	sorter.Swap(2, 2)
-	assert.Equal(t, ids[2], IDList[2])
+	if got, want := ids[2], IDList[2]; !reflect.DeepEqual(got, want) {
+		t.Error("ids[2], IDList[2]")
+	}
 }
 
 func TestSort(t *testing.T) {
@@ -320,5 +394,7 @@ func TestSort(t *testing.T) {
 		ids = append(ids, id)
 	}
 	Sort(ids)
-	assert.Equal(t, ids, []ID{IDList[1], IDList[2], IDList[0]})
+	if got, want := ids, []ID{IDList[1], IDList[2], IDList[0]}; !reflect.DeepEqual(got, want) {
+		t.Fail()
+	}
 }
